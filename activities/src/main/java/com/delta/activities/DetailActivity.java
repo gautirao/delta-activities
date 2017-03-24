cc2a7f406ad3b09b39ad39d761d3e30f38d0f6a8
Goutham <goutham.k.rao@gmail.com>
2017-03-24 10:30:43 +0000

    added logic to perform an implicity activity

--- activities/src/main/java/com/delta/activities/DetailActivity.java
+++ activities/src/main/java/com/delta/activities/DetailActivity.java
@@ -2,6 +2,9 @@ package com.delta.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,6 +12,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.net.URI;
+import java.util.List;
 
 /**
  * Created by grao on 23/03/2017.
@@ -18,6 +24,7 @@ public class DetailActivity extends Activity {
 
     private Spinner mSpinner = null;
     private Button mReturnButton = null;
+    private Button mPerformButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +52,53 @@ public class DetailActivity extends Activity {
             }
         });
 
+        mPerformButton = (Button)findViewById(R.id.performImplicit);
+        mPerformButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = mSpinner.getSelectedItemPosition();
+                Intent implicitIntent = null;
+
+                switch(position){
+
+                    case 0:
+                        break;
+                    case 1:
+                        implicitIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.co.uk"));
+                        break;
+                    case 2: // call some one
+                        implicitIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:(+91)9886966047"));
+                        break;
+                    case 3: // map
+                        implicitIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:38.124,-97.123"));
+                        break;
+                    case 4: //take a picture
+                        implicitIntent = new Intent("android.media.action.IMAGE_CAPTURE");
+                        break;
+                    case 5: // edit first contact
+                        implicitIntent= new Intent(Intent.ACTION_EDIT,Uri.parse("content://contacts/people/1"));
+                        break;
+
+                }
+
+                if(implicitIntent!=null){
+                    if(isIntentAvailable(implicitIntent)){
+                        startActivity(implicitIntent);
+                    }else{
+                        Toast.makeText(view.getContext(),"no application is available",Toast.LENGTH_SHORT);
+                    }
+                }
+            }
+        });
+
+    }
+
+    private boolean isIntentAvailable(Intent implicitIntent) {
+
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(implicitIntent,0);
+        boolean isIntentSafe = activities.size()>0;
+        return isIntentSafe;
     }
 
     @Override
